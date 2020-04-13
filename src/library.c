697a7e71efc455f1fe5f4ea6760e1ce98dedb9e8
Nicholas Patrick Dullam <ndullam@data.cs.purdue.edu>
2020-04-13 16:22:50 -0400

    Commit proj1

--- src/library.c
+++ src/library.c
@@ -222,7 +222,7 @@ void make_library(const char *path) {
   int status = 0;
   do {
     status = ftw(path, (void *) ftw_insert, 1);
-    assert(status != DUPLICATE_SONG-1;)
+    assert(status != DUPLICATE_SONG-1);
     printf("\n%d\n", status);
   } while (status != 0 && status != -1);
 }
